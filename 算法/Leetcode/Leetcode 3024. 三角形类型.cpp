724f785ff8ae03cd8006f2d2ac02c45e8fe67072
DISASTER-RUMBLING <2644935110@qq.com>
2025-05-19 22:58:34 +0800

    all

---
+++ 算法/Leetcode/Leetcode 3024. 三角形类型.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	string triangleType(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		int x = nums[0], y = nums[1], z = nums[2];
+		if (x + y <= z) {
+			return "none";
+		}
+		if (x == y && y == z) {
+			return "equilateral";
+		}
+		else if (x == y || y == z || x == z) {
+			return "isosceles";
+		}
+		return "scalene";
+	}
+};

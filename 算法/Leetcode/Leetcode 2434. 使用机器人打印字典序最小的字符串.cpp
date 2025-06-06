5a5659a3c528d3e18ea26531d50fd56ef52ed2d5
DISASTER-RUMBLING <2644935110@qq.com>
2025-06-06 22:59:49 +0800

    all

---
+++ 算法/Leetcode/Leetcode 2434. 使用机器人打印字典序最小的字符串.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<algorithm>
+#include<stack>
+
+using namespace std;
+
+bool Check(int*  chnum, char now) {
+	for (int i = 1; i < (int)(now - 'a'); i++) {
+		if (chnum[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+string robotWithString(string s) {
+	int now = 0, num = -1, sz = s.size();
+	string res="";
+	char arr[100005];
+	int chnum[27] = { 0 };
+	for (int i = 0; i < sz; i++) {
+		chnum[(int)(s[i] - 'a')] += 1;
+	}
+	while (res.size() < sz) {
+		if((num == -1 && now<sz)) {
+			arr[++num] = s[now];
+			chnum[(int)(s[now++] - 'a')] -= 1;
+		}
+		else if (!Check(chnum, arr[num]) || sz==now) {
+			res += arr[num--];
+		}
+		else if(now<sz){
+			arr[++num] = s[now];
+			chnum[(int)(s[now++] - 'a')] -= 1;
+		}
+	}
+	return res;
+}
+
+int main() {
+	robotWithString("zza");
+}
